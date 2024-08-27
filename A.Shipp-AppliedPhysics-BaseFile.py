f5dfb9aee2019ded0e53e1b2a0d525093f557c1d
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-08-27 14:52:28 -0500

    Base File
    Created a base file to quickly start new programs

---
+++ A.Shipp-AppliedPhysics-BaseFile.py
@@ -0,0 +1,23 @@
+"""
+Code written by AJ Shipp
+Applied Physics (PH306-01)
+"""
+
+####
+# Constants
+#===
+
+
+####
+# Initializing Functions
+#===
+
+
+####
+# Work
+#===
+
+
+####
+# Output Statements
+#===
