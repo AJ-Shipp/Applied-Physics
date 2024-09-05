96907631d61067701910d5d01505ce5ac7ad64f8
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-09-05 16:54:40 -0500

    Finalized
    - Finished the 2.4 assignment, and added comments

--- Unit-3/A.Shipp-AppliedPhysics-2.4.py
+++ Unit-3/A.Shipp-AppliedPhysics-2.4.py
@@ -6,22 +6,23 @@ Applied Physics (PH306-01)
 ####
 # Packages
 #===
+import numpy as np
 
 ####
 # Variables & Constants
 #===
-
-
-####
-# Initializing Functions
-#===
+numCurr = int(0)                # Pseudo-iterator variable
+numA = int(0)                   # Startup value 1
+numB = int(1)                   # Startup value 2
 
 
 ####
 # Work
 #===
+print('The terms in the Fibonacci sequence below 1000 are as follows: ')
 
-
-####
-# Output Statements
-#===
+while numCurr < 1000:
+    print(numCurr)
+    numCurr = numA + numB
+    numA = numB
+    numB = numCurr
