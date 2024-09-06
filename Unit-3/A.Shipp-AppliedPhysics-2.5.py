1b7a5aee10a73046fc2f3f190229dca65eca198f
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-09-05 19:04:10 -0500

    Finished
    - Program parses through the first 3 upper energy levels and the lower 5 energy levels corresponding to each.
    - Then it prints these two numbers, and their correspond wavelength.

--- Unit-3/A.Shipp-AppliedPhysics-2.5.py
+++ Unit-3/A.Shipp-AppliedPhysics-2.5.py
@@ -13,18 +13,24 @@ import astropy.constants as cn
 ####
 # Variables & Constants
 #===
-constRyd = cn.Ryd
+constRyd = cn.Ryd               #:Rydberg constant 
+lmb = float()                   #:Variable used for the wavelengths
+m = int()                       #:Principal quantum number of lower energy level
+n = int()                       #:Principal quantum number of upper energy level
 
 ####
 # Initializing Functions
 #===
-
+def findLambda(mInput, nInput):
+    lmb = 1/(constRyd*((1/mInput**2)-(1/nInput**2)))        #:Puts m&n numbers into the Rydberg formula, then returns the wavelength
+    return lmb
 
 ####
 # Work
 #===
-
-
-####
-# Output Statements
-#===
+for i in range(1,4):                #:Parses through the first 3 principal quantum numbers of the upper energy level
+    for j in range(i+1,i+6):        #:Parses through each corresponding first 5 principal quantum numbers of the lower energy level
+        lmb = findLambda(i,j)       #:Calls the findLambda function using the upper/lower energy levels' current values
+        print('For m=%i and n=%i, the wavelength is: %s' %(i,j,lmb))    #Outputs the information calculated
+        j += 1          #Iterates to the next lower energy level
+    i += 1              #Iterates to the next upper energy level
