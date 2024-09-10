59cab1b82619db33768370bf9fc3817f1cab8a99
AJ-Shipp <anthonyjamesshipp@gmail.com>
2024-09-10 14:53:00 -0500

    3.2 Base File
    - Created the base file for assignment 3.2

---
+++ Module-5/A.Shipp-AppliedPhysics-3.2.py
@@ -0,0 +1,27 @@
+"""
+Code written by AJ Shipp
+Applied Physics (PH306-01)
+"""
+
+####
+# Packages
+#===
+
+####
+# Variables & Constants
+#===
+
+
+####
+# Initializing Functions
+#===
+
+
+####
+# Work
+#===
+
+
+####
+# Output Statements
+#===
